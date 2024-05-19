b28f70ba395288a94d7229c3c7d76f21943c419c
IvanMolokin <ivan.molokin@gmail.com>
2024-05-19 20:46:20 +0300

    Задание 1 (Протокол Диффи-Хелмана)

--- server.py
+++ server.py
@@ -1,15 +1,36 @@
 import socket
 import pickle
+import random
 
 HOST = '127.0.0.1'
 PORT = 8080
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+with socket.socket() as sock:
+    sock.bind((HOST, PORT))
+    sock.listen(1)
+    conn, addr = sock.accept()
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+    # Принимаем параметры от клиента
+    msg = conn.recv(1024)
+    p, g, A = pickle.loads(msg)
 
-conn.close()
+    # Генерируем секретный ключ сервера
+    b = random.randint(1, p-1)
+    B = pow(g, b, p)
+
+    # Вычисляем общий секрет
+    K = pow(A, b, p)
+    print("Общий секрет:", K)
+
+    # Отправляем параметры клиенту
+    conn.send(pickle.dumps((p, g, B)))
+
+    # Принимаем и расшифровываем сообщение
+    encrypted_message = pickle.loads(conn.recv(1024))
+    message = ''.join([chr(pow(char, K, p)) for char in encrypted_message])
+    print("Получено сообщение:", message)
+
+    # Шифруем и отправляем ответ
+    response = "Привет, клиент!"
+    encrypted_response = [pow(ord(char), K, p) for char in response]
+    conn.send(pickle.dumps(encrypted_response))
